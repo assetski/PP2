329c2e487c93b2c181b11fbf1c6b161b78ebec20
Akimgereyev Asset <chicha99mu@gmail.com>
2019-02-13 04:15:52 +0600

    new

---
+++ week1/task3/lab1_3/lab1_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string[] arr = n.Split(); // split разбивает строку на элементы массива
+            for (int i=0;i<arr.Length;i++) 
+            {
+                for(int j = 0; j < 2; j++) // дублирование элемента массива
+                {
+                    Console.Write("{0} ", arr[i]); // {0} - это номер параметра. Вместо {0} при выводе будет использовано значение переменной
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
