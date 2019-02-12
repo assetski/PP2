4833f41f728ad55f77d85a9cf4b4912ddfd1e4d9
Akimgereyev Asset <chicha99mu@gmail.com>
2019-02-13 00:43:40 +0600

    qwer

---
+++ week1/task4/lab1_4/lab1_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "[*]";
+            int a = int.Parse(Console.ReadLine());  //  Ввод размера
+            for (int i = 1; i <= a; i++)  //      используем цикл в цикле
+            {                               
+                for (int j = 1; j <= i; j++)    
+                {
+                    Console.Write(s);  // выводим возрастающее каждой итерацией startriangle
+                }
+                Console.WriteLine();   //  переход в следующую строку
+            }
+            Console.ReadLine();  //  для того чтобы консоль не закрывалась
+        }
+    }
+}
