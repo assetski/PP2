886017119de2007f2a144cccde030ed973da4202
Akimgereyev Asset <chicha99mu@gmail.com>
2019-02-14 01:54:53 +0600

    a

---
+++ week2/task3/lab2_3/lab2_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab2_3
+{
+    class Program
+    {
+        static string Space(int a) // функция для пробелов 
+        {
+            string s = "      ";
+            string p = "";
+            for (int i = 0; i < a; i++)
+            {
+                p += s;
+            }
+            return p;
+        }
+        static void info(DirectoryInfo dir, int a) // рекурсия для открытия папок и файлов в виде дерева
+        {
+            FileInfo[] files = dir.GetFiles();
+            DirectoryInfo[] dires = dir.GetDirectories();
+            foreach (FileInfo file in files) // вывод названия файлов с определенным расстоянием
+            {
+
+                Console.Write(Space(a));
+                Console.WriteLine(file.Name);
+            }
+            foreach (DirectoryInfo dire in dires) // вывод названия папок с определенным расстоянием
+            {
+                Console.Write(Space(a)); // переход к функции Space
+                Console.WriteLine(dire.Name);
+                info(dire, a + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Akimgereyev\Desktop\галерея iphone");
+            info(dir, 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week2/task4/lab2_4/lab2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\Users\Akimgereyev\Desktop\asset\path1.txt";    //создание текстовых файлов в папке
+            string path2 = @"C:\Users\Akimgereyev\Desktop\asset\path2.txt";
+            string text = "manchester united";
+            StreamWriter sw = new StreamWriter(path1); // инструмент для ввода в первый файл
+            sw.Write(text);                            //ввод в первый файл
+            sw.Close();                                 
+            File.Copy(path1, path2);                    //копирование из первого во второй
+            File.Delete(path1);                         //удаление первого файла
+        }
+    }
+}
